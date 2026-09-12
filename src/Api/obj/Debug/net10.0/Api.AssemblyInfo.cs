9cfbf4210bc620c378c1573e5b188cdbf1ef0143
johnvgomes <john.v.gomes@gmail.com>
2026-09-12 13:51:56 -0300

    feat: initial clean architecture solution setup with .NET 10

--- src/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56da7063253ce91ee804ffe57abf34f867620a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1628e9dca88636a36358cd19ade4d6e2e188ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56da7063253ce91ee804ffe57abf34f867620a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1628e9dca88636a36358cd19ade4d6e2e188ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
